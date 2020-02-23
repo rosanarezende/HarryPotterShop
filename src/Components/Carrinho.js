d6de7500dbda11ee98443aee19bf66b277777420
Rosana Rezende <contato@rosanarezende.com>
2020-02-23 12:52:34 -0300

    Estrutura

---
+++ src/Components/Carrinho.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const CarrinhoContainer = styled.div``
+
+function Carrinho() {
+  return (
+    <CarrinhoContainer>
+      Eu sou o carrinho
+    </CarrinhoContainer>
+  );
+}
+
+export default Carrinho;
---
+++ src/Components/Filtro.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const FiltroContainer = styled.div``
+
+function Filtro() {
+  return (
+    <FiltroContainer>
+      Eu sou o filtro
+    </FiltroContainer>
+  );
+}
+
+export default Filtro;
