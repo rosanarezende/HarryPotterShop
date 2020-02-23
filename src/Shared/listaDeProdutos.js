aaffd2c225dbbad52150320847ca005af2ce7cb4
Rosana Rezende <contato@rosanarezende.com>
2020-02-23 12:41:28 -0300

    Initial commit

---
+++ src/Shared/listaDeProdutos.js
@@ -0,0 +1,59 @@
+export const LISTADEPRODUTOS =
+    [
+        {
+            id: 1,
+            name: "Cama do Astronauta",
+            value: 500.0,
+            imageUrl: "https://user-images.githubusercontent.com/45580434/75055787-68f5b100-54b4-11ea-849f-bcac9d4df84d.jpg",
+            descricao: "Com a Cama do Astronauta, a decoração do quarto dos pequenos será uma verdadeira aventura pelo espaço.",
+        },
+        {
+            id: 2,
+            name: "Lego Miniaturas",
+            value: 150.49,
+            imageUrl: "https://user-images.githubusercontent.com/45580434/75055804-6e52fb80-54b4-11ea-9bef-5451ec0477d1.png",
+            descricao: "Lego Miniaturas: 10 personagens diferentes para colecionar",
+        },
+        {
+            id: 3,
+            name: "Nave inflável",
+            value: 299.99,
+            imageUrl: "https://user-images.githubusercontent.com/45580434/75055807-6eeb9200-54b4-11ea-8469-5aafdc7ab5ec.jpg",
+            descricao: "Com o ônibus espacial inflável 'Jr Space Explorer' as crianças podem usar a imaginação para viajar para galáxias distantes, estrelas e planetas!",
+        },
+        {
+            id: 4,
+            name: "Cabana espacial",
+            value: 280.0,
+            imageUrl: "https://user-images.githubusercontent.com/45580434/75055779-67c48400-54b4-11ea-9fc7-eaa86ac1d408.jpg",
+            descricao: "A barraca pop-up que serve como posto de comando interplanetário para jovens exploradores espaciais.",
+        },
+        {
+            id: 5,
+            name: "Foguete lego",
+            value: 120.25,
+            imageUrl: "https://user-images.githubusercontent.com/45580434/75055801-6dba6500-54b4-11ea-949d-5994697bca97.jpg",
+            descricao: "Prepare-se para o lançamento do Foguete de Espaço Intersideral da plataforma de lançamento inspirada na NASA com cientistas e astronautas.",
+        },
+        {
+            id: 6,
+            name: "Lego City",
+            value: 499.99,
+            imageUrl: "https://user-images.githubusercontent.com/45580434/75055791-698e4780-54b4-11ea-879d-9e2db9ec711d.png",
+            descricao: "Envolva o seu filho no entusiasmante mundo das viagens espaciais com este conjunto para brincar sensacional.",
+        },
+        {
+            id: 7,
+            name: "Foguete Espacial",
+            value: 179.0,
+            imageUrl: "https://user-images.githubusercontent.com/45580434/75055800-6bf0a180-54b4-11ea-9729-45b133741b27.png",
+            descricao: "Inspire brincadeira espacial e científica com um conjunto para brincar fabuloso com ônibus de pesquisa e carro lunar.",
+        },
+        {
+            id: 8,
+            name: "Miniaturas StarWars",
+            value: 999.99,
+            imageUrl: "https://user-images.githubusercontent.com/45580434/75055808-6f842880-54b4-11ea-8496-4aebb9df797d.jpg",
+            descricao: "Miniatura Lego de personagens de StarWars",
+        },
+    ]
